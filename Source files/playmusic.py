aa85069f3e08d7299134b3bfcaf3a0273084bc41
sherryadan <adanmohammad80@gmail.com>
2024-05-26 10:07:01 +0500

    New Commit

--- Source files/playmusic.py
+++
@@ -1,5 +0,0 @@
-import pywhatkit
-
-def play_song(song_name):
-    # Play the song on YouTube
-    pywhatkit.playonyt(song_name)
--- Source files/searchbrowser.py
+++
@@ -1,5 +0,0 @@
-import pywhatkit
-
-def search_query(query):
-    # Search the query on Google
-    pywhatkit.search(query)
